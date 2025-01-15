5fd63bb584b8a4cb0831622afc0864b4d8463cc4
Ana Isabel <anaisabell.pb@gmail.com>
2025-01-15 19:30:23 +0000

    v

--- PHP/05-eliminar.php
+++ PHP/05-eliminar.php
@@ -1,50 +1,25 @@
-<!-- http://localhost/Curso/PHP/03-insertar.php -->
+<!-- http://localhost/Curso/PHP/02-consultar.php -->
 
 <?php
 // Llamar a errores y funciones
 require("errores.php");
 require("funciones.php");
 
-/* Recogemos datos del formulario */
+//Llamamos a la base de datos
+$conexion = conectarBBDD();
+
+
+//Hacemos la consulta
+$consulta = "SELECT * FROM productos";
+$filas = $conexion->query($consulta);
+$numfilas = $filas->num_rows;
+
+// Recogemos datos del formulario 
 $alerta = "Mensaje...";
 
 // Solo si se envía el formulario, se definen las variables del alert
 if (isset($_REQUEST['enviar'])) {
-    //Llamamos a la base de datos
-    $conexion = conectarBBDD();
-
-    $referencia = $_REQUEST['Referencias'] ?? '';
-    $descripcion = $_REQUEST['Descripcion'] ?? '';
-    $precio = $_REQUEST['Precio'] ?? '';
-    $stock = $_REQUEST['Stock'] ?? '';
-
-    // En el caso de categorias mando un array
-    $categorias = $_REQUEST['Categorias'] ?? [];
-
-    // implode sirve para escribir los elementos del array
-    $categoriasValores = implode(', ', $categorias);
-    $alerta = " Referencia: $referencia
-        Descripcion: $descripcion
-        Precio: $precio
-        Stock: $stock
-        Categorias: $categoriasValores";  // valor array
-
-    // Ahora introducimos lo de arriba en la BBDD
-    // Defino una sentencia preparada para evitar hakeos, se encriptan poniendo ?, por cada valor y la sentencia
-    // la 1º encriptación es con el prepare y la 2º es con la sustitución de las letras, aqui es idis.
-    $sentenciaSQL = "INSERT INTO productos 
-                     (Referencia, Descripcion, Precio, Stock, Categorias)
-                    VALUES (?,?,?,?,?)";
-    $sentenciaPreparada = $conexion->prepare($sentenciaSQL);
-    // Encriptamos la sentencia (bind_param)
-    $sentenciaPreparada->bind_param("isdis", $referencia, $descripcion, $precio, $stock, $categoriasValores);
-    // ejecute es booleano; true (correcto) false (error)
-    $ejecutaSQL = $sentenciaPreparada->execute();
-    if ($ejecutaSQL) {
-        $alerta .= "<br> Producto insertado correctamente";
-    } else {
-        $alerta .= "<br> ERROR FATAL (explota!)";
-    }
+    $alerta = "Nº de Registros: " . $numfilas;
 }
 ?>
 
@@ -67,63 +42,67 @@ if (isset($_REQUEST['enviar'])) {
             <?php echo $alerta; ?>
         </p>
     </header>
+    <!-- En el class defino con bootstrap el bg es el fondo primary el color-->
+    <section class="container align-center m-3 w-70 bg-primary">
+        <!-- Poner php en medio del html se le llama inyección de scrip-->
+        <?php
+        // Si se envia el formulario, cargo la tabla
+        if (isset($_REQUEST['enviar'])) {
+        ?>
+            <table class="table">
+                <thead>
+                    <th>Referencia</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Stock</th>
+                    <th>Categorías</th>
+                </thead>
+                <tbody>
+                    <?php
+                    // Asocia la salida a su campo
+                    $productos = $filas->fetch_all(MYSQLI_ASSOC);
+                    foreach ($productos as $producto) {
+
+                    ?>
+                        <tr>
+                            <td><?php echo $producto['Referencia']; ?></td>
+                            <td><?php echo $producto['Descripcion']; ?></td>
+                            <td><?php echo $producto['Precio']; ?></td>
+                            <td><?php echo $producto['Stock']; ?></td>
+                            <td><?php echo $producto['Categorias']; ?></td>
+                        </tr>
+
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+        <?php
+        }
+        ?>
+    </section>
 
     <!-- Línea de separación -->
     <hr class="m-3 border border-primary border-5 w-50">
 
     <form action="#" method="post" class="m-3 shadow-lg">
-        <!-- -->
-        <!--campo REFERECNIA-->
-        <label for="Referencia" class="form-label">Referencia:</label>
-        <input type="number" name="Referencia" id="Referencia"
-            class="form-control w-50" required><br>
-
-        <!-- -->
-        <!--Campo DESCRIPCIÓN -->
-        <label for="Descripcion" class="form-label">Descripción:</label>
-        <input type="text" name="Descripcion" id="Descripcion"
-            class="form-control w-50" required><br>
-
-        <!-- -->
-        <!--campo PRECIO-->
-        <label for="Precio" class="form-label">Precio:</label>
-        <input type="number" name="Precio" id="Precio"
-            class="form-control w-50" step="0.05" required><br>
-        <!-- -->
-        <!--campo STOCK-->
-        <label for="Stock" class="form-label">Stock:</label>
-        <input type="number" name="Stock" id="Stock"
-            class="form-control w-50" step="0.05" required><br>
-        <!-- -->
-        <!-- Campo CATEGORÍAS -->
-        <label class="form-label">Categorías</label><br>
-        <input type="checkbox" name="categorias[]" value="papelaría" id="papelaría">
-        <label for="papelería">Papelería</label><br>
-        <input type="checkbox" name="categorias[]" value="escolar" id="escolar">
-        <label for="escolar">Escolar</label><br>
-        <input type="checkbox" name="categorias[]" value="ofcina" id="ofcina">
-        <label for="ofcina">Ofcina</label><br>
-        <!-- -->
-        <hr class="border border-primary border-5 w-50">
-        <!-- -->
-        <button type="submit" class="btn btn-success" name="enviar">Enviar</button>
+        <button type="submit" class="btn btn-success" name="enviar">Consultar</button>
     </form>
     <!-- Defino enlaces de navegación con estilo boostrap-->
-    <section class="row">
+     <section class="row">
         <nav class="col">
             <a href="01-cargar-bbdd.php"
-                class="btn btn-sm btn-warning w-100">Cargar BBDD</a>
-            <a href="02-consultar.php"
-                class="btn btn-sm btn-success w-100">Consultar productos</a>
+            class="btn btn-sm btn-success w-100">Cargar BBDD</a>
+            <a href="03-insertar-bbdd.php"
+            class="btn btn-sm btn-warning w-100">Insertar producto</a>
         </nav>
         <nav class="col">
             <a href="04-actualizar-bbdd.php"
-                class="btn btn-sm btn-secondary w-100">Actualizar producto</a>
+            class="btn btn-sm btn-secondary w-100">Actualizar producto</a>
             <a href="05-eliminar-bbdd.php"
-                class="btn btn-sm btn-danger w-100">Eliminar producto</a>
+            class="btn btn-sm btn-danger w-100">Eliminar producto</a>
         </nav>
-    </section>
-
+     </section>
 </body>
 
 </html>
