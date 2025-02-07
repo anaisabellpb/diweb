ef49ac9a4c81046310f469d5501b808ae4a76baf
Ana Isabel <anaisabell.pb@gmail.com>
2025-02-07 20:20:28 +0000

    wp

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,27 @@
-<?php
-phpinfo ();
-?>
+- Extensiones Wordpress recomendadas
+
+
+ 
+  - https://wordpress.org/plugins/cookiebot/
+  - https://wordpress.org/plugins/jetpack/
+  - https://wordpress.org/plugins/disable-comments/
+  - https://wordpress.org/plugins/breadcrumb-navxt/
+  - https://es.wordpress.org/plugins/google-site-kit/
+
+  - https://es.wordpress.org/plugins/simple-social-buttons/
+  - https://es.wordpress.org/plugins/equalweb/
+  - https://es.wordpress.org/plugins/youtube-embed-plus/
+
+  - https://es.wordpress.org/plugins/classic-widgets/
+   
+- Extensiones de PAGO
+  - https://www.elegantthemes.com/gallery/divi/
+  
+  
+  INSTALADOS
+  
+  - https://es.wordpress.org/plugins/tinymce-advanced/
+  - https://es.wordpress.org/plugins/classic-editor/
+  - https://es.wordpress.org/plugins/polylang/
+  - https://es.wordpress.org/plugins/beaver-builder-lite-version/
+  - https://es.wordpress.org/plugins/contact-form-7/
