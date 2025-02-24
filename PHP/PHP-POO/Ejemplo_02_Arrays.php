adf96eb83386d25e94eaf0139b42fa666598a43b
Ana Isabel <anaisabell.pb@gmail.com>
2025-02-24 20:08:52 +0000

    PHP P002

--- PHP/PHP-POO/Ejemplo_02_Arrays.php
+++ PHP/PHP-POO/Ejemplo_02_Arrays.php
@@ -1,6 +1,40 @@
 <?php //PHP-POO/Ejemplo_02_Arrays.php
 /*Arrays Unidimensionales también se llaman Vectores */
+# Array en formato clásico, antiguo el que esta abajo: 
+$frutas = array("Peras", "Fresas", "Kiwis");
+$mensaje = "";
+// Recorremos el array con foreach
+// $variable = array, $key = indice: $value = elemento
+foreach ($frutas as $indice => $fruta) {
+    $mensaje .= "$fruta <br>";
+}
 
+# Array formato moderno
+$elementos =["Camion", true, 12, 3.1416];
+$mensaje .= "Array moderno <br>";
+$elementos [0] ="Coche";
+//foreach ($elementos as $indice => $elemento) {
+    //$mensaje .= "[$indice] -> $elemento <br>";
+//}
+# Añadimos un elemento al array
+$elementos [] = 5000;// Con este corchete vacío decimos de poner el valor al final de la array
+# array_splice sirve para quitar/poner elementos al array
+# Ponemos entre el 12 (2) y 3.1416 (3) el elemento "Moto"
+array_splice($elementos, 2, 0, "Moto");
+foreach ($elementos as $indice => $elemento) {
+    $mensaje .= "[$indice] -> $elemento <br>";
+}
+# Arrays asociativos
+$alumna = [
+    "nombre" => "Ana",
+    "edad" => 30,
+    "aprobada" => true
+];
+$alumna["edad"] = 31;
+$mensaje .= "Array Asociativo <br>";
+foreach ($alumna as $campo=> $dato) {
+    $mensaje .= "[$campo] => $dato <br>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="eS">
